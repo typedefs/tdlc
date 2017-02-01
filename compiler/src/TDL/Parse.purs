e3c42680c237acf2c4b46a5f14f5d831c3171de9
rightfold <rightfold@gmail.com>
2017-02-01 16:51:36 +0100

    Use new composite type syntax

--- compiler/src/TDL/Parse.purs
+++ compiler/src/TDL/Parse.purs
@@ -7,6 +7,7 @@ import Control.MonadZero (guard)
 import Data.Array as Array
 import Data.Either (Either)
 import Data.Foldable (foldr)
+import Data.List ((:), List(Nil))
 import Data.String as String
 import Data.Tuple.Nested ((/\))
 import Prelude
@@ -36,19 +37,25 @@ type_ :: Parser Type
 type_ = pure unit >>= \_ -> type' unit
 
 type' :: Unit -> Parser Type
-type' _ = do
-  head <-     P.try (NamedType <$> identifier)
-          <|> P.try (PrimType <$> primType)
-          <|> (ProductType <$> (leftBracePunc   *> fields <* rightBracePunc))
-          <|> (SumType     <$> (leftBracketPunc *> fields <* rightBracketPunc))
-  pure head
-  where fields = Array.fromFoldable <$> (field `PC.sepEndBy` commaPunc)
-        field  = (/\) <$> identifier <*> (colonPunc *> type_)
-
-primType :: Parser PrimType
-primType =     (I32Type       <$ i32Keyword)
-           <|> (F64Type       <$ f64Keyword)
-           <|> (TextType      <$ textKeyword)
+type' _ =
+      P.try (ProductType <<< Array.fromFoldable <$> field `sepEndBy1SepMandatory` asteriskPunc)
+  <|> P.try (SumType     <<< Array.fromFoldable <$> field `sepEndBy1SepMandatory` plusPunc)
+  <|>       simple
+  where
+    field = P.try $ (/\) <$> identifier <*> (colonPunc *> simple)
+    simple =     P.try (NamedType <$> identifier)
+             <|>       keywordType
+             <|>       (leftParenPunc *> type_ <* rightParenPunc)
+    keywordType =     (PrimType I32Type  <$ i32Keyword)
+                  <|> (PrimType F64Type  <$ f64Keyword)
+                  <|> (PrimType TextType <$ textKeyword)
+                  <|> (ProductType []    <$ unitKeyword)
+                  <|> (SumType     []    <$ voidKeyword)
+    sepEndBy1SepMandatory p sep = do
+      a <- p
+      (do sep
+          as <- PC.sepEndBy p sep
+          pure (a : as)) <|> ((a : Nil) <$ sep)
 
 --------------------------------------------------------------------------------
 
@@ -76,7 +83,7 @@ lexeme p = blank *> p <* blank
 identifier :: Parser String
 identifier = lexeme do
   name <- String.fromCharArray <<< Array.fromFoldable <$> PC.many1 PS.alphaNum
-  guard (name `Array.notElem` ["f64", "i32", "text", "type"])
+  guard (name `Array.notElem` ["f64", "i32", "text", "type", "unit", "void"])
   pure name
 
 f64Keyword :: Parser Unit
@@ -91,35 +98,29 @@ textKeyword = void $ lexeme $ PS.string "text"
 typeKeyword :: Parser Unit
 typeKeyword = void $ lexeme $ PS.string "type"
 
+unitKeyword :: Parser Unit
+unitKeyword = void $ lexeme $ PS.string "unit"
+
+voidKeyword :: Parser Unit
+voidKeyword = void $ lexeme $ PS.string "void"
+
 asteriskPunc :: Parser Unit
 asteriskPunc = void $ lexeme $ PS.char '*'
 
 colonPunc :: Parser Unit
 colonPunc = void $ lexeme $ PS.char ':'
 
-commaPunc :: Parser Unit
-commaPunc = void $ lexeme $ PS.char ','
-
 equalsSignPunc :: Parser Unit
 equalsSignPunc = void $ lexeme $ PS.char '='
-
-leftBracePunc :: Parser Unit
-leftBracePunc = void $ lexeme $ PS.char '{'
-
-leftBracketPunc :: Parser Unit
-leftBracketPunc = void $ lexeme $ PS.char '['
 
 leftParenPunc :: Parser Unit
 leftParenPunc = void $ lexeme $ PS.char '('
 
+plusPunc :: Parser Unit
+plusPunc = void $ lexeme $ PS.char '+'
+
 rightArrowPunc :: Parser Unit
 rightArrowPunc = void $ lexeme $ PS.string "->"
-
-rightBracePunc :: Parser Unit
-rightBracePunc = void $ lexeme $ PS.char '}'
-
-rightBracketPunc :: Parser Unit
-rightBracketPunc = void $ lexeme $ PS.char ']'
 
 rightParenPunc :: Parser Unit
 rightParenPunc = void $ lexeme $ PS.char ')'
